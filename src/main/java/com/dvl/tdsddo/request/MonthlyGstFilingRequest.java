14615595287baf42b1bb084888eed385dcdf40dd
Biswajit Ksnalabs <biswagithub@gmail.com>
2026-01-01 15:05:40 +0530

    Update MonthlyGstFilingRequest.java

--- src/main/java/com/dvl/tdsddo/request/MonthlyGstFilingRequest.java
+++ src/main/java/com/dvl/tdsddo/request/MonthlyGstFilingRequest.java
@@ -1,5 +1,6 @@
 package com.dvl.tdsddo.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -13,6 +14,7 @@ public class MonthlyGstFilingRequest {
     private Integer ddoId;
     private String filingMonth;
     private String arnNo;
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate arnDate;
 
     private BigDecimal declaredAmount;
